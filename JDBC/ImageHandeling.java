6c5339a8dec30aab621ae6b2ef957712052b6486
luminous44 <aklemasultanakhi@gmail.com>
2025-04-27 09:18:47 +0600

    making file dire in drive anf retrive image from db using jdbc

--- JDBC/ImageHandeling.java
+++ JDBC/ImageHandeling.java
@@ -1,14 +1,18 @@
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.Thread.Builder.OfVirtual;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ImageHandeling {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException,FileNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException,IOException {
         
         String url = "jdbc:mysql://localhost:3306/my_db";
         String user = "root";
@@ -16,7 +20,8 @@ public class ImageHandeling {
 
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection(url, user, password);
-        storeImage(con);
+        //storeImage(con);
+         getImage(con);
           
     }
 
@@ -39,5 +44,27 @@ public class ImageHandeling {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void getImage(Connection con) throws SQLException, IOException{
+        String folderPath = "D:\\images\\";
+        File file = new File(folderPath);
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        byte[] image;
+        PreparedStatement pst = con.prepareStatement("SELECT image FROM imagetable WHERE imgId = ?");
+        pst.setInt(1, 3);
+        ResultSet rs = pst.executeQuery();
+        if(rs.next()){
+            image = rs.getBytes("image");
+            String imagePath = folderPath+"flower.jpg";
+            OutputStream out = new FileOutputStream(imagePath);
+            out.write(image);
+        }else{
+            System.out.println("image not found");
+        }
+      
+
     }
 }
