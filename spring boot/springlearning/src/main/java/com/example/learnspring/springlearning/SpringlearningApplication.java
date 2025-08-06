de512e59095147b68d4f9d427005bcba20ed2919
luminous44 <aklemasultanakhi@gmail.com>
2025-08-06 21:27:33 +0600

    set up

---
+++ spring boot/springlearning/src/main/java/com/example/learnspring/springlearning/SpringlearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnspring.springlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlearningApplication.class, args);
+	}
+
+}
