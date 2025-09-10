0c15a105a9adf4a379fae0f03123d6c38748a5ab
luminous44 <aklemasultanakhi@gmail.com>
2025-09-10 08:51:02 +0600

    ps-6 maximum consecutive one

---
+++ StriversDSASheet/Array/Easy/ConsecutiveOne.java
@@ -0,0 +1,23 @@
+public class ConsecutiveOne {
+    
+    public static void main(String[] args) {
+
+        int arr[] = {1,1,0,1,1,1};
+        
+        int count = 0;
+        int max = Integer.MIN_VALUE;
+
+        for(int val : arr){
+            if(val == 1){
+                count +=1;
+                max = Math.max(count, max);
+            }else{
+             
+                count = 0;
+            }
+        }
+
+        System.out.println("Maximum consecutive one is : "+max);
+
+    }
+}
