8cba3f0b1003c4f0cd5260b7589c9579fbd2768b
luminous44 <aklemasultanakhi@gmail.com>
2025-09-11 21:17:37 +0600

    ps-10 Find element that appears once

---
+++ StriversDSASheet/Array/Easy/NumberAppersOnce.java
@@ -0,0 +1,12 @@
+public class NumberAppersOnce {
+    
+    public static void main(String[] args) {
+        int arr[] = {1,1,2,2,3,3,4,4,5};
+        int x = 0;
+         for(int val : arr){
+            x = x^val;
+        }
+
+        System.out.println("missing number is : "+x);
+    }
+}
