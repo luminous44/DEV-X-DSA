ce43373cd24539c80c12c1a43871d822f5d99b67
luminous44 <aklemasultanakhi@gmail.com>
2025-09-21 12:15:15 +0600

    Iteration in Thymeleaf

--- spring boot/springlearning/src/main/java/com/example/learnspring/springlearning/Controller/HomeController.java
+++ spring boot/springlearning/src/main/java/com/example/learnspring/springlearning/Controller/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
@@ -13,7 +15,19 @@ public class HomeController {
         model.addAttribute("name","Aklema Sultana");
         model.addAttribute("age",25);
 
+        List<String> disf = List.of("M1","Thorfin","No One","June","Kormosuchi","Homeless","Arique");
+        model.addAttribute("fn",disf);
+
         return "about";
+
+    }
+    @GetMapping("/home")
+    public String home(Model model) {
+
+        List<String> disf = List.of("M1","Thorfin","No One","June","Kormosuchi","Homeless","Arique");
+        model.addAttribute("fn",disf);
+
+        return "home";
 
     }
 
