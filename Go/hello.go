fb5f7af5d7257820ec00569f61ba3e54a76c2e0c
luminous44 <aklemasultanakhi@gmail.com>
2025-10-30 19:34:14 +0600

    variables

--- Go/hello.go
+++ Go/hello.go
@@ -4,9 +4,17 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Welcome to Go Programming Language")
-	fmt.Println(1 + 1)
-	fmt.Println(true)
-	fmt.Println(10.5)
-	fmt.Println(10.0/3.0)
+	// fmt.Println("Welcome to Go Programming Language")
+	// fmt.Println(1 + 1)
+	// fmt.Println(true)
+	// fmt.Println(10.5)
+	// fmt.Println(10.0/3.0)
+
+	// variables
+
+	//var name string = "Aklema"
+
+	//var name = "Sultana"
+	 name := "Akhi"
+	fmt.Println(name)
 }
