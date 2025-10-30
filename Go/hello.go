6f54aa9986baf9859fd813cac72573b286024d0a
luminous44 <aklemasultanakhi@gmail.com>
2025-10-30 21:02:26 +0600

    switch in go

--- Go/hello.go
+++ Go/hello.go
@@ -1,84 +1,84 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 const gender = "Female"
 func main() {
 
-	// fmt.Println("Welcome to Go Programming Language")
-	// fmt.Println(1 + 1)
-	// fmt.Println(true)
-	// fmt.Println(10.5)
-	// fmt.Println(10.0/3.0)
+	fmt.Println("Welcome to Go Programming Language")
+	fmt.Println(1 + 1)
+	fmt.Println(true)
+	fmt.Println(10.5)
+	fmt.Println(10.0/3.0)
 
-	// variables
+	variables
 
-	//var name string = "Aklema"
+	var name string = "Aklema"
 
-	//var name = "Sultana"
-	//  name := "Akhi"
+	var name = "Sultana"
+	 name := "Akhi"
 
-	// var name string
-	// name = "Akhi" 
-	// fmt.Println(name)
-    // constant
-	// const age = "26"
-    // fmt.Println(age)
+	var name string
+	name = "Akhi" 
+	fmt.Println(name)
+    constant
+	const age = "26"
+    fmt.Println(age)
 
-    //  fmt.Println(gender)
+     fmt.Println(gender)
 
-	//  const (
+	 const (
 
-	// 	port = 8080
+		port = 8080
 
-	// 	host = "localhost"
-	//  )
+		host = "localhost"
+	 )
 
-	//  fmt.Println(port,host)
+	 fmt.Println(port,host)
 
-	// loop --> for is the only construct in go
+	loop --> for is the only construct in go
 
-    //  while loop
-	// i := 0
+     while loop
+	i := 0
 
-	// for i<=3 {
-	// 	fmt.Println(i)
-	// 	i++
-	// }
+	for i<=3 {
+		fmt.Println(i)
+		i++
+	}
 
-	// // for loop
-	// for i := 0; i < 3; i++ {
-	//   if i == 2 {
-	// 	continue
-	//   }
-    //    fmt.Println(i)
-	// }
+	// for loop
+	for i := 0; i < 3; i++ {
+	  if i == 2 {
+		continue
+	  }
+       fmt.Println(i)
+	}
 
-	// // infinity loop
+	// infinity loop
 
-	// for {
-	// 	 fmt.Println("i")
-	// }
+	for {
+		 fmt.Println("i")
+	}
 
-	// for i := range 11 {
+	for i := range 11 {
 
-	//    fmt.Println(i)
-	// }
+	   fmt.Println(i)
+	}
 
-	// if else
+	if else
 
-	// age := 18
+	age := 18
 
-	// if age >= 18 {
+	if age >= 18 {
 
-	// 	fmt.Println("Person is an Adult")
-	// } else {
-	// 	fmt.Println("Person is not an Adult")
-	// }
-
-	//if else-if
-
+		fmt.Println("Person is an Adult")
+	} else {
+		fmt.Println("Person is not an Adult")
+	}
+	if else-if
 	age := 6
-
 	if age >= 18 {
 
 		fmt.Println("Person is an Adult")
@@ -109,5 +109,42 @@ func main() {
 	} else {
 		fmt.Println("Person is not a Kid",age)
 	}
+
+	// simple switch
+
+	i := 2
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("Two")
+	default :
+	 fmt.Println("Other")
+	}
+
+	// multiple condition switch
+
+	switch time.Now().Weekday(){
+	case time.Friday, time.Saturday :
+		fmt.Println("It's Weekend")
+    default:
+		fmt.Println("It's WorkDay")
+	}
+
+	whoAmI := func (i interface{})  {
+
+		switch i.(type){
+		case int:
+			fmt.Println("Integer")
+		case bool:
+			fmt.Println("Boolean")
+		case string:
+			fmt.Println("String")
+		default:
+			fmt.Println("Other")
+	 }
+	}
+	whoAmI(1.0)
+
 
 }
