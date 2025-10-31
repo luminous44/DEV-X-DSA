06fab00e7b857027be3ddc6d1ca169439c860348
luminous44 <aklemasultanakhi@gmail.com>
2025-10-31 10:11:27 +0600

    function in go

---
+++ Go/functions/functions.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func add(a int, b int) int{
+	return a + b
+}
+func lang()(string,string,int){
+	return "go","lang",12
+}
+
+func processIt() func(a int) int{
+    
+	return func(a int) int {
+		return 4;
+	}
+}
+func main() {
+	res := add(2, 3)
+	fmt.Println(res)
+	fmt.Println(lang())
+	l1,l2,in1 := lang() //store in variable
+	fmt.Println(l1,l2,in1)
+	l11,l22,_ := lang()
+	fmt.Println(l11,l22) // _ unused variable
+
+
+	fn := processIt()
+	fmt.Println(fn)
+
+}
