14af18d85a3093b5100be857fc827885b8d1f057
luminous44 <aklemasultanakhi@gmail.com>
2025-10-31 10:56:25 +0600

    pointer in go

---
+++ Go/pointer/pointers.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func change(num *int) {
+
+	*num = 5
+
+}
+
+func main() {
+   
+	num := 2
+    fmt.Println("before change :", num)
+	change(&num)
+	fmt.Println("After Change :", num)
+
+
+}
