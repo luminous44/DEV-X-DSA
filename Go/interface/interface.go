8fd69bd1cc3ef6aab7269d540dae29128306c6a8
luminous44 <aklemasultanakhi@gmail.com>
2025-11-02 11:12:21 +0600

    adde fk gateway

--- Go/interface/interface.go
+++ Go/interface/interface.go
@@ -1,17 +1,26 @@
 package main
 
-type payment struct{}
+type payment struct{
+	
+}
 
 func (p payment) makePayment(amount int){
 
 }
 
 type bkash struct{}
+type nogot struct{}
+type fkGateway struct{}
 
-func (b bkash) pay(amount int){}
+func (b bkash) pay(amount float32){}
 
+func (n nogot) pay(amount float32){}
+
+
+func (fg fkGateway) pay(amount float32)
 	
 func main(){
+ 
 
 
 }
