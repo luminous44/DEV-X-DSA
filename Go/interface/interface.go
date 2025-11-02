26c523b1e5715384dc3729e2893925e7fa467cee
luminous44 <aklemasultanakhi@gmail.com>
2025-11-02 11:58:09 +0600

    making payment using gateway

--- Go/interface/interface.go
+++ Go/interface/interface.go
@@ -1,10 +1,15 @@
 package main
 
-type payment struct{
-	
+import "fmt"
+
+type payment struct {
 }
 
-func (p payment) makePayment(amount int){
+func (p payment) makePayment(amount int) {
+    
+	paymentGateWay := bkash{}
+
+	paymentGateWay.pay(float32(amount))
 
 }
 
@@ -12,15 +17,22 @@ type bkash struct{}
 type nogot struct{}
 type fkGateway struct{}
 
-func (b bkash) pay(amount float32){}
+func (b bkash) pay(amount float32) {
+		fmt.Println("making payment using Bkash : ", amount)
+}
 
-func (n nogot) pay(amount float32){}
+func (n nogot) pay(amount float32) {
 
+	fmt.Println("making payment using Nogot : ", amount)
+}
 
-func (fg fkGateway) pay(amount float32)
-	
-func main(){
- 
+func (fg fkGateway) pay(amount float32){
+	fmt.Println("making payment using nogot : ", amount)
+}
 
+func main() {
+
+	payment := payment{}
+	payment.makePayment(25.0)
 
 }
