daaaf0605c739e344847b9e9a3106599237f12cc
luminous44 <aklemasultanakhi@gmail.com>
2025-11-03 12:16:48 +0600

    channels using function

--- Go/Channels/channels.go
+++ Go/Channels/channels.go
@@ -2,31 +2,42 @@ package main
 
 import (
 	"fmt"
-	"math/rand/v2"
-	"time"
-	//"time"
+	// "math/rand/v2"
+	// "time"
+	// //"time"
 )
 
-
-func processNum(numChan chan int){
-
-
-	for num :=range numChan{
-      	fmt.Println("processing number :", num)
-		time.Sleep(time.Second)
-	}
+// // sending data 
+// func processNum(numChan chan int){
 
 
+// 	for num :=range numChan{
+//       	fmt.Println("processing number :", num)
+// 		time.Sleep(time.Second)
+// 	}
+
+
+
+// }
+
+func sum( result chan int,num1 int, num2 int){
+	numResult := num1 + num2
+	result <- numResult
 
 }
 func main() {
+   result := make(chan int)
 
-     numChan := make(chan int)
-	 go processNum(numChan)
+   go sum (result,4,5)
+
+   res := <- result
+   fmt.Println(res)
+    //  numChan := make(chan int)
+	//  go processNum(numChan)
 	
-	 for {
-		numChan <- rand.IntN(1000)
-	 }
+	//  for {
+	// 	numChan <- rand.IntN(1000)
+	//  }
 
    // time.Sleep(time.Second*2)
 //    //channels are blocking 
