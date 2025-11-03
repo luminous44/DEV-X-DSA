78e2a393172487b1e7479c8b5b00afbd94e74f77
luminous44 <aklemasultanakhi@gmail.com>
2025-11-03 11:44:26 +0600

    waitGroup in grroutines

--- Go/Goroutines/goroutines.go
+++ Go/Goroutines/goroutines.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
+	//"time"
 )
 
-// func task(id int) {
-// 	fmt.Println("doing task",id)
-// }
+func task(id int, w *sync.WaitGroup) {
+	defer w.Done() // defer run after all work done in function
+	fmt.Println("doing task",id)
+}
 
 func main() {
-   
+   var wg sync.WaitGroup
+
 	for i:= 0; i<= 10; i++ {
-		// go task(i)
-		go func(i int)  {
-			fmt.Println(i)
-		}(i)
+		wg.Add(1)
+		go task(i,&wg)
+		// go func(i int)  {
+		// 	fmt.Println(i)
+		// }(i)
 	}  
-	time.Sleep(time.Second * 2)
+	//time.Sleep(time.Second * 2)
+
+   wg.Wait()
 }
