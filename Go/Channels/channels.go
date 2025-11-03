c958c39d1d23a696c3687702bcada5b1679fd8f5
luminous44 <aklemasultanakhi@gmail.com>
2025-11-03 12:09:46 +0600

    channel with random number

--- Go/Channels/channels.go
+++ Go/Channels/channels.go
@@ -2,31 +2,40 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
 	"time"
-
-
+	//"time"
 )
 
 
 func processNum(numChan chan int){
 
-	fmt.Println("processing number :", <-numChan)
+
+	for num :=range numChan{
+      	fmt.Println("processing number :", num)
+		time.Sleep(time.Second)
+	}
+
+
 
 }
 func main() {
 
      numChan := make(chan int)
 	 go processNum(numChan)
-	  numChan <- 20
+	
+	 for {
+		numChan <- rand.IntN(1000)
+	 }
 
-    time.Sleep(time.Second*2)
-   channels are blocking 
-	messageChannel := make(chan string)
-	messageChannel <- "ping" // sending data
+   // time.Sleep(time.Second*2)
+//    //channels are blocking 
+// 	messageChannel := make(chan string)
+// 	messageChannel <- "ping" // sending data
 
-	msg := <-messageChannel // receive  data
+// 	msg := <-messageChannel // receive  data
 
-	fmt.Println(msg)
+// 	fmt.Println(msg)
  
 
 }
