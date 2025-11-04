ab7f59661b466ccc9a9e5565a8b53f9d3de1ec26
luminous44 <aklemasultanakhi@gmail.com>
2025-11-04 19:19:24 +0600

    mutex in go

---
+++ Go/Mutex/mutexx.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type post struct {
+	views int
+	mu sync.Mutex
+}
+
+func (p *post) inc( wg *sync.WaitGroup) {
+     defer func ()  {
+		 wg.Done() 
+		p.mu.Unlock()
+		
+	 }()
+	 p.mu.Lock()
+	 p.views +=1
+	
+
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	myPost := post{views: 0}
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+			go myPost.inc(&wg)
+	}
+
+	wg.Wait()
+
+	fmt.Println(myPost.views)
+}
