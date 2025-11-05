7eb18ea4ade002fdb16d7adac127751f08436550
luminous44 <aklemasultanakhi@gmail.com>
2025-11-05 12:08:10 +0600

    final commit of file

--- Go/Files/files.go
+++ Go/Files/files.go
@@ -8,135 +8,135 @@ import (
 
 func main() {
 fmt.Println()
-//  f, err :=	os.Open("example.txt")
+ f, err :=	os.Open("example.txt")
 
-//  if err != nil {
+ if err != nil {
 	
-// 	panic(err)
-//  }
+	panic(err)
+ }
 
-//  fileInfo, err := f.Stat()
+ fileInfo, err := f.Stat()
 
-//  if err != nil {
-// 	panic(err)
-//  }
-//  fmt.Println("file name : ", fileInfo.Name())
-//  fmt.Println("file or folder : ", fileInfo.IsDir())
-//  fmt.Println("file size : ", fileInfo.Size())
-//  fmt.Println("file permission : ",fileInfo.Mode())
-//  fmt.Println("file modification at : ",fileInfo.ModTime())
+ if err != nil {
+	panic(err)
+ }
+ fmt.Println("file name : ", fileInfo.Name())
+ fmt.Println("file or folder : ", fileInfo.IsDir())
+ fmt.Println("file size : ", fileInfo.Size())
+ fmt.Println("file permission : ",fileInfo.Mode())
+ fmt.Println("file modification at : ",fileInfo.ModTime())
 
 // read file
  
-// f, err :=	os.Open("example.txt")
+f, err :=	os.Open("example.txt")
 
-//  defer f.Close()
+ defer f.Close()
 
-//  buf :=make([]byte, 15)
+ buf :=make([]byte, 15)
 
-//  d, err := f.Read(buf)
+ d, err := f.Read(buf)
  
-//  if err != nil {
+ if err != nil {
 	
-// 	panic(err)
-//  }
+	panic(err)
+ }
 
-//  fmt.Println("data : ",d,buf)
+ fmt.Println("data : ",d,buf)
  
-//  for i := 0; i < len(buf); i++ {
-// 	 fmt.Println("data : ",d,string(buf[i]))
+ for i := 0; i < len(buf); i++ {
+	 fmt.Println("data : ",d,string(buf[i]))
 	
-//  }
+ }
 
-//  data, err :=	os.ReadFile("example.txt")
+ data, err :=	os.ReadFile("example.txt")
 
-//  if err != nil {
+ if err != nil {
 	
-// 	panic(err)
-//  }
+	panic(err)
+ }
 
-//  fmt.Println(string(data))
+ fmt.Println(string(data))
  
  //read folder
  
-//  dir,err :=os.Open("../")
+ dir,err :=os.Open("../")
 
-//  if err != nil {
+ if err != nil {
 	
-// 	panic(err)
-//  }
+	panic(err)
+ }
 
-//  defer dir.Close()
+ defer dir.Close()
 
-//  fileInfo, err := dir.ReadDir(-1)
+ fileInfo, err := dir.ReadDir(-1)
 
-//  for _, fi := range fileInfo {
-// 	fmt.Println(fi.Name(),fi.IsDir())
-//  }
+ for _, fi := range fileInfo {
+	fmt.Println(fi.Name(),fi.IsDir())
+ }
 
  // create file
 
-//  f, err := os.Create("example2.txt")
+ f, err := os.Create("example2.txt")
 
  
-//  if err != nil {
+ if err != nil {
 	
-// 	panic(err)
-//  }
+	panic(err)
+ }
   
-//  defer f.Close()
+ defer f.Close()
 
-// //  f.WriteString("hi go")
-// //  // appending 
-// // f.WriteString(" nice language")
+//  f.WriteString("hi go")
+//  // appending 
+// f.WriteString(" nice language")
 
-// bytes := []byte("hello golang")
+bytes := []byte("hello golang")
 
-// f.Write(bytes)
+f.Write(bytes)
 
 // data read and write one to another file(streaming fashion)
 
-// sourceFile, err := os.Open("example.txt")
+sourceFile, err := os.Open("example.txt")
 
-// if err != nil{
-// 	panic(err)
-// }
+if err != nil{
+	panic(err)
+}
 
-// defer sourceFile.Close()
+defer sourceFile.Close()
 
-// destFile, err := os.Create("example2.txt")
+destFile, err := os.Create("example2.txt")
 
-// if err != nil {
-// 	panic(err)
-// }
+if err != nil {
+	panic(err)
+}
 
-// defer destFile.Close()
+defer destFile.Close()
 
-// reader := bufio.NewReader(sourceFile)
+reader := bufio.NewReader(sourceFile)
 
-// Writer := bufio.NewWriter(destFile)
+Writer := bufio.NewWriter(destFile)
 
-// for  {
+for  {
 
-// 	b, err := reader.ReadByte()
+	b, err := reader.ReadByte()
 	
-// if err != nil {
+if err != nil {
 
-// 	if err.Error() != "EOF"{
-//       	panic(err)
-// 	}
-//   break
-// }
+	if err.Error() != "EOF"{
+      	panic(err)
+	}
+  break
+}
 
-// 	e := Writer.WriteByte(b)
+	e := Writer.WriteByte(b)
 
-// 	if err != nil {
-// 	panic(e)
-// }
+	if err != nil {
+	panic(e)
+}
 
-// }
+}
 
-// Writer.Flush()
+Writer.Flush()
 
 // fmt.Println("written to new file")
 
