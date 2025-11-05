b43c71579a7608c7737306672ababf4a04597b63
luminous44 <aklemasultanakhi@gmail.com>
2025-11-05 11:29:59 +0600

    file reading

--- Go/Files/files.go
+++ Go/Files/files.go
@@ -7,22 +7,46 @@ import (
 
 func main() {
 
- f, err :=	os.Open("example.txt")
+//  f, err :=	os.Open("example.txt")
 
+//  if err != nil {
+	
+// 	panic(err)
+//  }
+
+//  fileInfo, err := f.Stat()
+
+//  if err != nil {
+// 	panic(err)
+//  }
+//  fmt.Println("file name : ", fileInfo.Name())
+//  fmt.Println("file or folder : ", fileInfo.IsDir())
+//  fmt.Println("file size : ", fileInfo.Size())
+//  fmt.Println("file permission : ",fileInfo.Mode())
+//  fmt.Println("file modification at : ",fileInfo.ModTime())
+
+// read file
+ 
+f, err :=	os.Open("example.txt")
+
+ defer f.Close()
+
+ buf :=make([]byte, 15)
+
+ d, err := f.Read(buf)
+ 
  if err != nil {
 	
 	panic(err)
  }
 
- fileInfo, err := f.Stat()
-
- if err != nil {
-	panic(err)
+ fmt.Println("data : ",d,buf)
+ 
+ for i := 0; i < len(buf); i++ {
+	 fmt.Println("data : ",d,string(buf[i]))
+	
  }
- fmt.Println("file name : ", fileInfo.Name())
- fmt.Println("file or folder : ", fileInfo.IsDir())
- fmt.Println("file size : ", fileInfo.Size())
- fmt.Println("file permission : ",fileInfo.Mode())
- fmt.Println("file modification at : ",fileInfo.ModTime())
+ 
+
 
 }
